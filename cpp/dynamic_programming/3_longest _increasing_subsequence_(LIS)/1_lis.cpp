41fbb93ccf69ba27f92ae897551e17dd0de4d7ac
Yusnier M. Sosa <yusnier.msv@gmail.com>
2021-06-27 21:13:07 -0600

    commit

---
+++ cpp/dynamic_programming/3_longest _increasing_subsequence_(LIS)/1_lis.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <vector>
+
+typedef std::vector<std::vector<int>> vector2d;
+
+// Returns the Longest Increasing Subsequence (LIS) on 'sequence' in O(n^2).
+// Reference: Competitive Programming 3, P. 105, Steven & Felix Halim.
+int lis_v1(const std::vector<int> &sequence) {
+    if (sequence.empty()) { return 0; }
+
+    // Let 'lis[i]' be the LIS ending at index 'i'.
+    std::vector<int> lis(sequence.size(), 1);
+
+    int max_subsequence = 1;
+    for (int i = 1; i < sequence.size(); ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (sequence[j] < sequence[i] && lis[j] + 1 > lis[i]) {
+                lis[i] = lis[j] + 1;
+            }
+        }
+        max_subsequence = std::max(max_subsequence, lis[i]);
+    }
+
+    return max_subsequence;
+}
+
+// Returns the Longest Increasing Subsequence (LIS) on 'sequence' in O(n^2).
+// In this variant we make the search to all 'j' from 'i' (for every j > i),
+// instead of from all j to i (for every j < i), like in v1 version.
+int lis_v2(const std::vector<int> &sequence) {
+    if (sequence.empty()) { return 0; }
+
+    // Let 'lis[i]' be the LIS ending at index 'i'.
+    std::vector<int> lis(sequence.size(), 1);
+
+    int max_subsequence = 1;
+    for (int i = 0; i < sequence.size(); ++i) {
+        for (int j = i + 1; j < sequence.size(); ++j) {
+            if (sequence[i] < sequence[j] && lis[i] + 1 > lis[j]) {
+                lis[j] = lis[i] + 1;
+                max_subsequence = std::max(max_subsequence, lis[j]);
+            }
+        }
+    }
+
+    return max_subsequence;
+}
+
+// Returns the Longest Increasing Subsequence (LIS) on 'sequence' in O(nlog(n)),
+// using a patient sort approach. This variant is not the most suitable to construct solutions.
+int lis_patient_sort(const std::vector<int> &sequence) {
+    // Set to apply patience sorting algorithm: <sequence value>.
+    std::set<int> lis;
+
+    for (const int value: sequence) {
+        const auto it = lis.lower_bound(value);
+        if (it != lis.end()) {
+            lis.erase(it);
+        }
+        lis.insert(value);
+    }
+
+    return static_cast<int>(lis.size());
+}
+
+void test_algorithms(const std::vector<int> &sequence) {
+    const std::set<int> lis = {
+            lis_v1(sequence),
+            lis_v2(sequence),
+            lis_patient_sort(sequence),
+    };
+    assert(lis.size() == 1);  // checking that all algorithms have the same result.
+    std::cout << "LIS: " << *lis.begin() << (*lis.begin() == 0 ? " -> No Solution" : "") << std::endl;
+}
+
+int main() {
+    test_algorithms({-7, 10, 9, 2, 3, 8, 8, 1});
+    std::cout << std::endl;
+    test_algorithms({0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15});
+    std::cout << std::endl;
+    test_algorithms({9, 6, 7, 8, 1, 2, 3, 0});
+    std::cout << std::endl;
+    test_algorithms({0, 1, 2, 3, 4, 5});
+    std::cout << std::endl;
+    test_algorithms({5, 4, 3, 2, 1, 0});
+    std::cout << std::endl;
+    test_algorithms({});
+}
---
+++ cpp/dynamic_programming/3_longest _increasing_subsequence_(LIS)/2_lis_with_solutions.cpp
@@ -0,0 +1,138 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <vector>
+
+typedef std::vector<std::vector<int>> vector2d;
+
+// Utility recursive function to construct LIS solutions.
+vector2d build_solutions(const std::vector<int> &sequence, const vector2d &parents, int index) {
+    // If 'sequence[index]' has no parents, then it is an initial value of a sequence.
+    if (parents[index].empty()) {
+        return {{sequence[index]}};
+    }
+    // Building all solutions traversing 'parents' tree recursively using a BFS approach.
+    vector2d solutions;
+    for (const int parent_index: parents[index]) {
+        vector2d sub_solutions = build_solutions(sequence, parents, parent_index);
+        for (auto &solution: sub_solutions) {
+            solution.push_back(sequence[index]);
+            solutions.push_back(std::move(solution));
+        }
+    }
+    return solutions;
+}
+
+// Variant of 'lis_v1' to return all the solutions for the LIS on 'sequence' in O(n^2).
+// Reference: Competitive Programming 3, P. 105, Steven & Felix Halim.
+vector2d lis_v1_solutions(const std::vector<int> &sequence) {
+    if (sequence.empty()) { return {}; }
+
+    // Let 'lis[i]' be the LIS ending at index 'i'.
+    std::vector<int> lis(sequence.size(), 1);
+    // 'parents[i]' is a list of parent values that precedes to sequence[i] in the LIS.
+    vector2d parents(sequence.size(), std::vector<int>());
+
+    int max_lis_size = 1;
+    for (int i = 1; i < sequence.size(); ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (sequence[j] < sequence[i]) {
+                if (lis[j] + 1 == lis[i]) {
+                    parents[i].push_back(j);
+                } else if (lis[j] + 1 > lis[i]) {
+                    lis[i] = lis[j] + 1;
+                    parents[i].clear();
+                    parents[i].push_back(j);
+                }
+            }
+        }
+        max_lis_size = std::max(max_lis_size, lis[i]);
+    }
+
+    vector2d result;
+    for (int i = 0; i < lis.size(); ++i) {
+        if (lis[i] == max_lis_size) {
+            vector2d solutions = build_solutions(sequence, parents, i);
+            std::move(solutions.begin(), solutions.end(), std::back_inserter(result));
+        }
+    }
+
+    return result;
+}
+
+// Variant of 'lis_v2' to return all the solutions for the LIS on 'sequence' in O(n^2).
+// In this variant we make the search to all 'j' from 'i' (for every j > i),
+// instead of from all j to i (for every j < i), like in v1 version.
+vector2d lis_v2_solutions(const std::vector<int> &sequence) {
+    if (sequence.empty()) { return {}; }
+
+    // Let 'lis[i]' be the LIS ending at index 'i'.
+    std::vector<int> lis(sequence.size(), 1);
+    // 'parents[i]' is a list of parent values that precedes to sequence[i] in the LIS.
+    vector2d parents(sequence.size(), std::vector<int>());
+
+    int max_lis_size = 1;
+    for (int i = 0; i < sequence.size(); ++i) {
+        for (int j = i + 1; j < sequence.size(); ++j) {
+            if (sequence[i] < sequence[j]) {
+                if (lis[i] + 1 == lis[j]) {
+                    parents[j].push_back(i);
+                } else if (lis[i] + 1 > lis[j]) {
+                    lis[j] = lis[i] + 1;
+                    max_lis_size = std::max(max_lis_size, lis[j]);
+                    parents[j].clear();
+                    parents[j].push_back(i);
+                }
+            }
+        }
+    }
+
+    vector2d result;
+    for (int i = 0; i < lis.size(); ++i) {
+        if (lis[i] == max_lis_size) {
+            vector2d solutions = build_solutions(sequence, parents, i);
+            std::move(solutions.begin(), solutions.end(), std::back_inserter(result));
+        }
+    }
+
+    return result;
+}
+
+void test_algorithms(const std::vector<int> &sequence) {
+    const std::set<vector2d> lis_solutions = {
+            lis_v1_solutions(sequence),
+            lis_v2_solutions(sequence),
+    };
+    assert(lis_solutions.size() == 1);  // checking that all algorithms have the same result.
+    if (lis_solutions.begin()->empty()) {
+        std::cout << "No Solution" << std::endl;
+        return;
+    }
+    std::cout << "LIS: " << lis_solutions.begin()->at(0).size() << std::endl;
+    std::cout << "Solutions: " << std::endl;
+    for (const auto &solution: *lis_solutions.begin()) {
+        std::cout << "[";
+        if (!solution.empty()) {
+            std::cout << solution[0];
+        }
+        for (int i = 1; i < solution.size(); ++i) {
+            std::cout << ", " << solution[i];
+        }
+        std::cout << "]" << std::endl;
+    }
+}
+
+int main() {
+    test_algorithms({-7, 10, 9, 2, 3, 8, 8, 1});
+    std::cout << std::endl;
+    test_algorithms({0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15});
+    std::cout << std::endl;
+    test_algorithms({9, 6, 7, 8, 1, 2, 3, 0});
+    std::cout << std::endl;
+    test_algorithms({0, 1, 2, 3, 4, 5});
+    std::cout << std::endl;
+    test_algorithms({5, 4, 3, 2, 1, 0});
+    std::cout << std::endl;
+    test_algorithms({});
+}
